6d11f712964b34bce76f75da7ed6617931a59a1e
e2tox <e2tox@live.com>
2019-10-16 18:44:04 +0800

    npm run dev

--- index.ts
+++ index.ts
@@ -1,13 +1,31 @@
+import { singleton } from '@agentframework/domain';
 import { controller, method, WebContext } from '@agentframework/web';
 
-//
+class HelloService {
+  hello() {
+    return 'Hello Agent Framework';
+  }
+}
+
+class UnusedService {
+  constructor() {
+    console.log('never reach this line');
+  }
+}
+
 @controller('/api')
 export class HelloController {
+  @singleton()
+  helloService: HelloService;
+
+  @singleton()
+  unusedService: UnusedService;
+
   /**
    * full url /api/hello
    */
   @method('GET', '/hello')
   async hello(ctx: WebContext) {
-    return 'Hello Agent Framework';
+    return this.helloService.hello();
   }
 }
